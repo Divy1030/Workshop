e426d945a88c2def57735670a189c749f6f3ca8e
Anaghjaiswar <jaiswaranagh@gmail.com>
2025-04-03 23:27:29 +0530

    created a simulated payment environment using testing api keys

---
+++ backend/registration/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from .models import Registration
+
+class RegistrationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Registration
+        fields = [
+            'id', 'full_name', 'student_number', 'branch', 'gender',
+            'phone', 'email', 'living_type',
+            'is_email_verified', 'payment_status', 'payment_reference',
+            'created_at', 'updated_at'
+        ]
+        read_only_fields = ['is_email_verified', 'payment_status', 'payment_reference', 'created_at', 'updated_at']
+
+        
