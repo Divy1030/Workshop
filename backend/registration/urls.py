34a5875145096fecf0b59dc90d006133d545405d
Anaghjaiswar <jaiswaranagh@gmail.com>
2025-04-04 17:24:33 +0530

    doing deployment

--- backend/registration/urls.py
+++ backend/registration/urls.py
@@ -4,7 +4,7 @@ from .views import RegistrationCreateView, EmailVerificationView , initiate_paym
 urlpatterns = [
     path('register/', RegistrationCreateView.as_view(), name='registration-create'),
     path('email-verify/', EmailVerificationView.as_view(), name='email-verify'),
-     path('payment/initiate/', initiate_payment, name='initiate-payment'),
+    path('payment/initiate/', initiate_payment, name='initiate-payment'),
     path('payment/webhook/', payment_webhook, name='payment-webhook'),
 ]
 
